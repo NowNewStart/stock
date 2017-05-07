4f3a94b51c2cb23d9c9874303b41368036722fc0
NowNewStart <j.gelmar@hotmail.de>
2017-05-08 01:55:29 +0200

    do some other tests

--- tests/Feature/BankTest.php
+++ tests/Feature/BankTest.php
@@ -5,20 +5,21 @@ namespace Tests\Feature;
 use App\Bank;
 use App\Transformers\BankTransformer;
 use Tests\TestCase;
+use App\User;
 
 class BankTest extends TestCase
 {
-    public function testBankIndex()
+    public function testCreateBankAccount()
     {
-        $this->createAuthenticatedUser();
-        $transformed = $this->transformData($this->user->bank, new BankTransformer());
-        $this->response = $this->callAuthenticated('GET', '/api/bank')->assertExactJson($transformed);
+        $user = factory(User::class)->create()->createBankAccount();
+        $this->assertEquals(10000000, $user->bank->credit);
     }
 
-    public function getBankTopUsers()
+    public function testGetBankTopUsers()
     {
-        $this->createAuthenticatedUser();
-        $transformed = $this->transformData(Bank::orderBy('credit', 'desc')->get()->take(10), new BankTransformer());
-        $this->response = $this->callAuthenticated('GET', '/api/bank/top/10')->assertExactJson($transformed);
+        $users = factory(User::class, 10)->create()->each(function ($user) {
+            $user->createBankAccount();
+            $user->credit = $user->bank->credit;
+        });
     }
 }
