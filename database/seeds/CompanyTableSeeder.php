2fd76d87482b9325489f4e4dad00ecafd9af087b
NowNewStart <j.gelmar@hotmail.de>
2017-05-16 18:33:31 +0200

    change seeder so the identifier stays unique

--- database/seeds/CompanyTableSeeder.php
+++ database/seeds/CompanyTableSeeder.php
@@ -12,6 +12,9 @@ class CompanyTableSeeder extends Seeder
             $company_name = Faker::create()->company;
             $shares = rand(10000, 10000000);
             $value = rand(0, 100) * 100;
+            while(Company::whereIdentifier(strtoupper(substr($company_name),0,4))->count() > 0) {
+                $company_name = Faker::create()->company;
+            }
             $company = Company::create([
                 'name'        => $company_name,
                 'identifier'  => strtoupper(substr($company_name, 0, 4)),
