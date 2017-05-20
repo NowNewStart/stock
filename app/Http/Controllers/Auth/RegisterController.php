435447c9e98241a03619b5746d0c80f2cc40dc66
NowNewStart <j.gelmar@hotmail.de>
2017-05-20 22:24:58 +0200

    fix register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name'     => $data['name'],
             'email'    => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+        $user->createBankAccount();
+        return $user;
     }
 }
