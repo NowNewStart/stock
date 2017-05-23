c13aa1961d6fff8a5d97862f09a5695eaf260d25
NowNewStart <j.gelmar@hotmail.de>
2017-05-23 17:34:59 +0200

    seeded companies do not start under 5$ per share now

--- database/seeds/CompanyTableSeeder.php
+++ database/seeds/CompanyTableSeeder.php
@@ -11,7 +11,7 @@ class CompanyTableSeeder extends Seeder
         for ($i = 0; $i < 100; $i++) {
             $company_name = Faker::create()->unique()->company;
             $shares = rand(10000, 10000000);
-            $value = rand(0, 100) * 100;
+            $value = rand(5, 100) * 100;
             while (Company::whereIdentifier(strtoupper(substr($company_name, 0, 4)))->count() > 0) {
                 $company_name = Faker::create()->unique()->company;
             }
